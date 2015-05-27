d78e45a9e042a48f4ecb922733b0a57921d80b07
hallnt <nickyb63@hotmail.com>
2015-05-26 23:44:01 -0400

    added hello world content to project

---
+++ COMP397-Lesson01-Part2/Scripts/game.js
@@ -0,0 +1,19 @@
+﻿var Person = (function () {
+    function Person() {
+    }
+    //CONSTRUCTOR++++++++++++++++++++++++
+    Person.prototype.Constructor = function () {
+    };
+
+    //PUBLIC METHODS+++++++++++++++++++++
+    Person.prototype.saysHello = function () {
+        console.log("hello world!");
+    };
+    return Person;
+})();
+
+function init() {
+    var myPerson = new Person();
+    myPerson.saysHello();
+}
+//# sourceMappingURL=game.js.map
---
+++ COMP397-Lesson01-Part2/Scripts/game.ts
@@ -0,0 +1,14 @@
+﻿class Person {
+    //CONSTRUCTOR++++++++++++++++++++++++
+    Constructor() {
+    }
+    //PUBLIC METHODS+++++++++++++++++++++
+    public saysHello() {
+        console.log("hello world!");
+    }
+} 
+
+function init() {
+    var myPerson = new Person();
+    myPerson.saysHello();
+}
